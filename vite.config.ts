4d9430ae8b0215197786467a1e309d7a8bb05298
ninosantos-code <josemarrosario26@gmail.com>
2026-03-13 02:17:34 -0300

    Update environment variable in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({mode}) => {
   return {
     plugins: [react(), tailwindcss()],
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.gen-lang-client-0842077200': JSON.stringify(env.gen-lang-client-0842077200),
     },
     resolve: {
       alias: {
